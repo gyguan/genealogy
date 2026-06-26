72b02f532101f4027afe96d8883f839df265c148
hhj679 <huhx08@qq.com>
2026-06-26 14:15:15 +0800

    feat: add generation scheme query methods

--- backend/genealogy-backend/src/main/java/com/genealogy/generation/repository/GenSchemeRepository.java
+++ backend/genealogy-backend/src/main/java/com/genealogy/generation/repository/GenSchemeRepository.java
@@ -3,5 +3,9 @@ package com.genealogy.generation.repository;
 import com.genealogy.generation.entity.GenerationSchemeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GenSchemeRepository extends JpaRepository<GenerationSchemeEntity, Long> {
+
+    List<GenerationSchemeEntity> findByClanIdOrderByIsDefaultDescIdAsc(Long clanId);
 }
