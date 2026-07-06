a9e4ef114a9f65f8c243b7a45586ba9004d70132
hhj679 <huhx08@qq.com>
2026-07-06 19:41:50 +0800

    feat: enable person code rule properties

--- backend/genealogy-backend/src/main/java/com/genealogy/GenealogyApplication.java
+++ backend/genealogy-backend/src/main/java/com/genealogy/GenealogyApplication.java
@@ -1,9 +1,12 @@
 package com.genealogy;
 
+import com.genealogy.person.application.PersonCodeRuleProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(PersonCodeRuleProperties.class)
 public class GenealogyApplication {
 
     public static void main(String[] args) {
