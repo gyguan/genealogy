985f48c87725169f8750c662901d769db34cd4af
gyguan <guangangyu@163.com>
2026-07-29 09:42:09 +0800

    refactor(tree): move graph SQL out of relationship CRUD repository

--- backend/genealogy-backend/src/main/java/com/genealogy/relationship/repository/RelationshipRepository.java
+++ backend/genealogy-backend/src/main/java/com/genealogy/relationship/repository/RelationshipRepository.java
@@ -1,16 +1,17 @@
 package com.genealogy.relationship.repository;
 
 import com.genealogy.relationship.entity.RelationshipEntity;
-import org.springframework.data.domain.Pageable;
+import com.genealogy.tree.repository.TreeRelationshipQueryRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
-public interface RelationshipRepository extends JpaRepository<RelationshipEntity, Long> {
+public interface RelationshipRepository extends
+        JpaRepository<RelationshipEntity, Long>,
+        TreeRelationshipQueryRepository {
 
     List<RelationshipEntity> findByFromPersonIdAndDeletedAtIsNull(Long fromPersonId);
 
@@ -21,65 +22,17 @@ public interface RelationshipRepository extends JpaRepository<RelationshipEntity
     Optional<RelationshipEntity> findByIdAndClanIdAndDeletedAtIsNull(Long id, Long clanId);
 
     @Query("select r from RelationshipEntity r where r.clanId = :clanId and r.fromPersonId = :fromId and r.toPersonId = :toId and r.relationType = :type and r.deletedAt is null")
-    List<RelationshipEntity> findActiveSameRelation(@Param("clanId") Long clanId, @Param("fromId") Long fromId, @Param("toId") Long toId, @Param("type") String type);
+    List<RelationshipEntity> findActiveSameRelation(
+            @Param("clanId") Long clanId,
+            @Param("fromId") Long fromId,
+            @Param("toId") Long toId,
+            @Param("type") String type
+    );
 
     @Query("select r from RelationshipEntity r where r.clanId = :clanId and r.toPersonId = :toId and r.relationType = :type and r.deletedAt is null")
-    List<RelationshipEntity> findActiveToRelations(@Param("clanId") Long clanId, @Param("toId") Long toId, @Param("type") String type);
-
-    @Query("""
-            select r
-            from RelationshipEntity r
-            where r.clanId = :clanId
-              and r.fromPersonId in :personIds
-              and r.dataStatus in :statuses
-              and r.deletedAt is null
-              and (:lineageOnly = false or r.relationType = 'parent_child' or r.isLineageRelation = true)
-              and r.relationCategory in :categories
-            order by r.fromPersonId, r.toPersonId, r.id
-            """)
-    List<RelationshipEntity> findTreeOutgoing(
+    List<RelationshipEntity> findActiveToRelations(
             @Param("clanId") Long clanId,
-            @Param("personIds") Collection<Long> personIds,
-            @Param("statuses") Collection<String> statuses,
-            @Param("categories") Collection<String> categories,
-            @Param("lineageOnly") boolean lineageOnly
-    );
-
-    @Query("""
-            select r
-            from RelationshipEntity r
-            where r.clanId = :clanId
-              and r.toPersonId in :personIds
-              and r.dataStatus in :statuses
-              and r.deletedAt is null
-              and (:lineageOnly = false or r.relationType = 'parent_child' or r.isLineageRelation = true)
-              and r.relationCategory in :categories
-            order by r.toPersonId, r.fromPersonId, r.id
-            """)
-    List<RelationshipEntity> findTreeIncoming(
-            @Param("clanId") Long clanId,
-            @Param("personIds") Collection<Long> personIds,
-            @Param("statuses") Collection<String> statuses,
-            @Param("categories") Collection<String> categories,
-            @Param("lineageOnly") boolean lineageOnly
-    );
-
-    @Query("""
-            select r
-            from RelationshipEntity r
-            where r.clanId = :clanId
-              and r.fromPersonId in :personIds
-              and r.toPersonId in :personIds
-              and r.dataStatus in :statuses
-              and r.deletedAt is null
-              and r.relationCategory in :categories
-            order by r.fromPersonId, r.toPersonId, r.id
-            """)
-    List<RelationshipEntity> findTreeWithinPeople(
-            @Param("clanId") Long clanId,
-            @Param("personIds") Collection<Long> personIds,
-            @Param("statuses") Collection<String> statuses,
-            @Param("categories") Collection<String> categories,
-            Pageable pageable
+            @Param("toId") Long toId,
+            @Param("type") String type
     );
 }
